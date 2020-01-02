1f718bf86ef6d45f04c21c1a835d93b8f6e119d6
Michael Wizner <mwz@users.noreply.github.com>
2020-01-02 00:05:30 +0000

    Update sbt-sonar.

--- scalaHighlight.js
+++ scalaHighlight.js
@@ -1,8 +1,6 @@
-import Highlight from "@theme/CodeBlock";
 import Prism from "prism-react-renderer/prism";
-import vars from "./vars.json";
 
 (typeof global !== "undefined" ? global : window).Prism = Prism;
 require("prismjs/components/prism-scala");
 
-export { vars, Prism, Highlight };
+export { Prism };
