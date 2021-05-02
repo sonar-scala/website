c5ca7a156c16f1bf2aa1395ec7dd407a4f5571c4
Michael Wizner <mwz@users.noreply.github.com>
2021-05-02 23:49:03 +0100

    Fix loading rules

--- nofs.js
+++ nofs.js
@@ -3,14 +3,32 @@ module.exports = function (context, options) {
     name: "nofs-plugin",
     configureWebpack(config, isServer, utils) {
       return {
+        module: {
+          rules: [
+            {
+              test: /node_modules\/vfile\/core\.js/,
+              use: [
+                {
+                  loader: "imports-loader",
+                  options: {
+                    type: "commonjs",
+                    imports: ["single process/browser process"],
+                  },
+                },
+              ],
+            },
+          ],
+        },
         resolve: {
           alias: {
             path: require.resolve("path-browserify"),
+            process: "process/browser",
           },
           fallback: {
             fs: false,
             http: require.resolve("stream-http"),
             https: require.resolve("https-browserify"),
+            process: false,
           },
         },
       };
