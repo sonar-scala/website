f3b31473229c3b47b0bf0c9c1d10f8440e19d89b
Michael Wizner <mwz@users.noreply.github.com>
2024-04-08 11:56:43 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,7 @@ module.exports = {
   title: "sonar-scala",
   tagline:
     "A free and open-source SonarQube plugin for static code analysis of Scala projects",
-  url: "https://sonar-scala.github.io/website/",
+  url: "https://sonar-scala.github.io/",
   baseUrl: "/website/",
   favicon: "img/favicon.ico",
   organizationName: "sonar-scala",
